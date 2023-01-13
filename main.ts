c7bd69f862f22fc74004578923cc463193b333e4
Saarthakk14 <122539546+Saarthakk14@users.noreply.github.com>
2023-01-13 08:59:13 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Asleep)
+    music.playMelody("E D G F B A C5 B ", 500)
 })
 input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.Skull)
